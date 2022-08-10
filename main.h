d1461c12b37f2cac87b84f5ab3575c8c2f5c6dd0
Obasi <104761686+obasitorti@users.noreply.github.com>
2022-08-10 08:00:14 +0100

    updates and corrections

--- main.h
+++ main.h
@@ -1,62 +1,75 @@
-#ifndef _MAIN_H_
-#define _MAIN_H_
-#include <sys/types.h>
-#include <sys/stat.h>
-#include <sys/wait.h>
-#include <stdlib.h>
+#ifndef MAIN_H
+#define MAIN_H
+
 #include <stdio.h>
-#include <unistd.h>
+#include <stdlib.h>
 #include <string.h>
+#include <unistd.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <sys/wait.h>
 
-/*==================================================*/
-/*============     Shell_Init       ==============*/
-/*==================================================*/
+#define BUFFER 1024
+#define TRUE 1
+#define PROMPT "$ "
+/* error strings */
+#define ERR_MALLOC "Unable to malloc space\n"
+#define ERR_FORK "Unable to fork and create child process\n"
+#define ERR_PATH "No such file or directory\n"
+extern char **environ;
 
-int main(int ac, char **av, char **env);
-void prompt(void);
-void handle(int signals);
-void _EOF(char *buffer);
-void shell_exit(char **command);
+/**
+ * struct list_s - linked list of variables
+ * @value: value
+ * @next: pointer to next node
+ *
+ * Description: generic linked list struct for variables.
+**/
+typedef struct list_s
+{
+	char *value;
+	struct list_s *next;
+} list_s;
 
-/*==================================================*/
-/*============     create_child       ==============*/
-/*==================================================*/
+/**
+ * struct built_s - linked list of builtins
+ * @name: name of builtin
+ * @p: pointer to function
+ *
+ * Description: struct for builtin functions.
+**/
+typedef struct built_s
+{
+	char *name;
+	int (*p)(void);
+} built_s;
 
-void create_child(char **command, char *name, char **env, int cicles);
-int change_dir(const char *path);
+void prompt(int fd, struct stat buf);
+char *_getline(FILE *fp);
+char **tokenizer(char *str);
+char *_which(char *command, char *fullpath, char *path);
+int child(char *fullpath, char **tokens);
+void errors(int error);
 
-/*==================================================*/
-/*============        Execute       ==============*/
-/*==================================================*/
-
-void execute(char **command, char *name, char **env, int cicles);
-void print_env(char **env);
-char **_getPATH(char **env);
-void msgerror(char *name, int cicles, char **command);
-
-/*==================================================*/
-/*============          Tokening      ==============*/
-/*==================================================*/
-
-char **tokening(char *buffer, const char *s);
-
-/*==================================================*/
-/*============     Free Memory      ==============*/
-/*==================================================*/
-
-void free_dp(char **command);
-void free_exit(char **command);
-
-/*==================================================*/
-/*============  Auxiliar_Functions    ==============*/
-/*==================================================*/
-
-int _strcmp(char *s1, char *s2);
-unsigned int _strlen(char *s);
+/* utility functions */
+void _puts(char *str);
+int _strlen(char *s);
+int _strcmp(char *name, char *variable, unsigned int length);
+int _strncmp(char *name, char *variable, unsigned int length);
 char *_strcpy(char *dest, char *src);
-int _atoi(char *s);
-char *_strcat(char *dest, char *src);
 
-/*============ END      ==============*/
+/* prototypes for builtins */
+int shell_env(void);
+int shell_exit(void);
+int builtin_execute(char **tokens);
+int shell_num_builtins(built_s builtin[]);
 
-#endif /* _MAIN_H_ */
+/* prototypes for the helper functions for path linked list */
+char *_getenv(const char *name);
+char **copy_env(char **environ_copy, unsigned int environ_length);
+list_s *pathlist(char *variable, list_s *head);
+
+/* prototypes for free functions */
+void free_all(char **tokens, char *path, char *line, char *fullpath, int flag);
+void free_dp(char **array, unsigned int length);
+#endif /* MAIN_H */
---
+++ tests/main_getline.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * main - uses getline and prints line
+ * Return: 0
+ */
+int main (void)
+{
+	char *line = NULL;
+
+	line = _getline(stdin, line);
+
+	printf("%s\n", line);
+
+	free(line);
+
+	return (0);
+}
